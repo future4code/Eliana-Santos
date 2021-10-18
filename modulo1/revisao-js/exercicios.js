f4a1215024b3744263882c1ab2cc4c8ee15d98be
Elianacris <ellianacris@hotmail.com>
2021-10-18 18:09:31 -0300

    exercicio 8

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -43,16 +43,21 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-    if(num1 > num2){
 
-    }
 
 }
-
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
+    let numerosPares = []
+    for (let i = 0; numerosPares.length < n; i++) {
+        if (i % 2 == 0) {
+            numerosPares.push(i)
+           
+        }
+    }return numerosPares;
 
 }
+
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
