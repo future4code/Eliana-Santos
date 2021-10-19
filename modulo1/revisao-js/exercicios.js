9e9218ca6b1bf91e36ff927a1bad8f359852252d
Elianacris <ellianacris@hotmail.com>
2021-10-18 21:04:18 -0300

    exercicio 12

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -88,20 +88,18 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-    // const filme = {
-    //     nome: 'O Diabo Veste Prada',
-    //     ano: 2006,
-    //     diretor: 'David Frankel',
-    //     atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
-    // }
-    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores}`
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.map((ator) => ` ${ator}`)}.`
 }
-
 
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-
+     pessoaAnonima = {
+        ...pessoa,
+        nome: "ANÔNIMO"
+        
+    }
+    return pessoaAnonima
 }
 
 // EXERCÍCIO 13A
