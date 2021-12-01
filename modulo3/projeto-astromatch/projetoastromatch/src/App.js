265553f90554fc849b659338a6e2e379ac681254
ellianacris <ellianacris@hotmail.com>
2021-12-01 15:56:46 -0300

    projeto astromatch em andamento

--- modulo3/projeto-astromatch/projetoastromatch/src/App.js
+++ modulo3/projeto-astromatch/projetoastromatch/src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import styled from 'styled-components'
+import { useState } from "react";
+import axios from 'axios'
+//import TelaMatch from "./components/TelaMatch/TelaMatch";
+
+
+
+const Card = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-top: 50px;
+padding: 50px;
+border: 1px solid black;
+width: 50%;
+
+`
 
 function App() {
+  // const [match, setMatch] = useState(false)
+  const [data, setDate] = useState({
+    profile: {
+      id: "",
+      age: 0,
+      name: "",
+      photo: "",
+      bio: ""
+    }
+  })
+
+  useEffect(() => {
+    getProfile();
+    cleanProfiles()
+  }, []);
+
+  const getProfile = () => {
+    axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/person/`)
+      .then((res) => {
+        setDate(res.data);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  }
+
+  const matchProfile = (id, choice) => {
+    axios.post(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/choose-person`,
+      {
+        "id": id,
+        "choice": choice
+      })
+      .then((res) => {
+        setDate(res.data);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  }
+
+  const cleanProfiles = () => {
+    axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/clear')
+      .then((res) => {
+        console.log('Clear profiles success!')
+      })
+      .catch((erro) => {
+        console.log('Error clear profiles success!', erro)
+      });
+  }
+
+  const onClickNoMatch = () => {
+    getProfile();
+  }
+
+  const onClickMatch = (id) => {
+    console.log(data.profile)
+    //matchProfile(id, true) 
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Card>
+        <div>
+          <div>
+            <button>Ver Match</button>
+          </div>
+          <img width="240" src={data.profile.photo} alt="imagens" />
+          <div><strong>{data.profile.name}</strong>,{data.profile.age}
+            <p>{data.profile.bio}</p>
+          </div>
+        </div>
+        <div>
+          <button onClick={onClickNoMatch}>Não</button>
+          <button onClick={onClickMatch}  >Sim</button>
+        </div>
+      </Card>
+    </div >
   );
 }
 
