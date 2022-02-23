b6f20607697af2066892d906a39d1855ce15a710
ellianacris <ellianacris@hotmail.com>
2022-02-23 20:45:44 -0300

    iniciando projeto cookenu, endpoints login e signUp criados e funcionando

---
+++ modulo5/projeto-cookenu/src/services/Authenticator.ts
@@ -0,0 +1,34 @@
+import * as jwt from "jsonwebtoken"
+import { USER_ROLES } from "../types/User";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export interface AuthenticationData {
+    id: string,
+    role: USER_ROLES
+}
+export class Authenticator {
+    generate(input: AuthenticationData): string {
+        const token = jwt.sign(input, process.env.JWT_SECRET as string, {
+            expiresIn: process.env.EXPIRES_IN,
+        })
+        return token;
+    }
+    getTokenData = (token: string) => {
+        try {
+            const tokenData = jwt.verify(
+                token,
+                process.env.JWT_SECRET as string
+            ) as jwt.JwtPayload
+
+            return {
+                id:tokenData.id,
+                role:tokenData.role
+            }
+        } catch (error) {
+            console.log(error)
+            return null
+        }
+    }
+}
