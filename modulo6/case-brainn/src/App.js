395eab8081fc09c7ed217ca93b177d08db4af5c9
ellianacris <ellianacris@hotmail.com>
2022-04-01 13:29:47 -0300

    alterações na estilização

--- modulo6/case-brainn/src/App.js
+++ modulo6/case-brainn/src/App.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
-import {
-  Box,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Typography,
-} from "@mui/material";
+import { Box, FormControl, MenuItem, Select, Typography } from "@mui/material";
 import Trevo from "./assets/trevo.png";
 import {
   getConcursos,
   getConcursosById,
   getLoterias,
 } from "./services/loterias";
+import { ThemeProvider } from "@material-ui/styles";
 
 function App() {
   const [loterias, setLoterias] = useState([]);
@@ -54,40 +48,31 @@ function App() {
   };
 
   return (
-    <Box
-      sx={{
-        display: "grid",
-        gridTemplateColumns: "repeat(12, 1fr)",
-        heigth: "100vh",
-        width: "100%",
-        m: 0,
-        p: 0,
-      }}
-    >
+    <ThemeProvider>
       <Box
-        // disableGutters={false}
         sx={{
-          gridColumn: "span 12",
-          //   rowGap: 1,
-          //columnGap: ,
-          backgroundColor: "#d3d3d3",
-          width: "100%",
-
+          display: "flex",
+          minHeight: "100vh",
+          //gridTemplateColumns: "repeat(12, 1fr)",
+          //heigth: "100vh",
+          //width: "100%",
           m: 0,
           p: 0,
-          //borderTopLeftRadius: "70%",
-          //borderBottomLeftRadius: "70%",
+          backgroundColor: "#6BEFA3",
+          // boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          //zIndex: 2,
         }}
       >
         <Box
           sx={{
-            gridColumn: "span 12",
+            //gridColumn: "span 12",
             backgroundColor: "#6BEFA3",
-            width: "100%",
+            width: "50vw",
             height: "100vh",
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            // boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
             p: 0,
             m: 0,
+            //  zIndex: 2
           }}
         >
           <FormControl>
@@ -112,8 +97,57 @@ function App() {
           </Typography>
           <p>{concurso.numeros}</p>
         </Box>
+        <Box
+          // disableGutters={false}
+          sx={{
+            //gridColumn: "span 12",
+            //   rowGap: 1,
+            //columnGap: ,
+            backgroundColor: "#d3d3d3",
+            width: "100vw",
+            m: 0,
+            p: 0,
+            borderTopLeftRadius: "40%",
+            borderBottomLeftRadius: "50%",
+            //zIndex: 9,
+            //border:'solid',
+            boxShadow: "2px 0px 2px 2px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexDirection: "row",
+            //justifyContent: "center",
+            // alignItems: "stretch",
+            //alingContent: "space-around",
+            alignContent: "center",
+            flexWrap: "wrap",
+            pl: "10%",
+            gap: "15px",
+            justifyContent: "center",
+          }}
+        >
+          {concurso.numeros.map((numero) => {
+            return (
+              <Box
+                sx={{
+                  borderRadius: "50%",
+                  backgroundColor: "#fff",
+                  // textAlign: "center",
+                  //boxShadow: "2px 2px 2px 2px rgba( 0,0,0, 0.25)",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  fontSize: "22px",
+                  fontWeight: "bold",
+                  width: "5rem",
+                  height: "5rem",
+                }}
+              >
+                {numero}
+              </Box>
+            );
+          })}
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 
