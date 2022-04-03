aeef6c1266f874667524b606da31f8fbe0d2cb4d
ellianacris <ellianacris@hotmail.com>
2022-04-03 15:57:31 -0300

    alterações na estilização

--- modulo6/case-brainn/src/App.js
+++ modulo6/case-brainn/src/App.js
@@ -7,6 +7,7 @@ import {
   getLoterias,
 } from "./services/loterias";
 import { ThemeProvider } from "@material-ui/styles";
+import theme from "./constants/theme";
 
 function App() {
   const [loterias, setLoterias] = useState([]);
@@ -48,35 +49,30 @@ function App() {
   };
 
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <Box
         sx={{
           display: "flex",
-          minHeight: "100vh",
-          //gridTemplateColumns: "repeat(12, 1fr)",
-          //heigth: "100vh",
-          //width: "100%",
-          m: 0,
-          p: 0,
           backgroundColor: "#6BEFA3",
-          // boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-          //zIndex: 2,
         }}
       >
         <Box
           sx={{
-            //gridColumn: "span 12",
+             display:'grid',
+            gridColumn: "span 6",
             backgroundColor: "#6BEFA3",
             width: "50vw",
             height: "100vh",
-            // boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            p: 0,
-            m: 0,
-            //  zIndex: 2
+            m:'5px'
           }}
         >
+          
           <FormControl>
-            <Select onChange={onChangeSelect} value={loteria.id}>
+            <Select
+              onChange={onChangeSelect}
+              value={loteria.id}
+              sx={{ backgroundColor: "#fff", borderRadius:'15px'}}
+            >
               {loterias.map((lot) => {
                 return (
                   <MenuItem key={lot.id} value={lot.id}>
@@ -89,34 +85,56 @@ function App() {
 
           <img src={Trevo} alt="trevo" />
 
-          <Typography variant="h5">{loteria.nome.toUpperCase()}</Typography>
+          <Typography
+            sx={{
+              fontFamily: "Open Sans",
+              fontSize: "30px",
+              color: "#fff",
+              fontStyle: "normal",
+              lineHeight: "37px",
+              fontWeight: 700,
+            }}
+          >
+            {loteria.nome.toUpperCase()}
+          </Typography>
 
-          <Typography variant="subtitle1">CONCURSO</Typography>
-          <Typography>
+          <Typography
+            variant="subtitle1"
+            sx={{
+              fontFamily: "Open Sans",
+              color: "#fff",
+              fontStyle: "normal",
+              lineHeight: "17px",
+              fontWeight: 500,
+              fontSize: "14px",
+            }}
+          >
+            CONCURSO
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "Open Sans",
+              color: "#fff",
+              fontStyle: "normal",
+              lineHeight: "24px",
+              fontWeight: 700,
+              fontSize: "20px",
+            }}
+          >
             {concurso.id} – {concurso.data}
           </Typography>
-          <p>{concurso.numeros}</p>
         </Box>
         <Box
-          // disableGutters={false}
           sx={{
             //gridColumn: "span 12",
-            //   rowGap: 1,
             //columnGap: ,
             backgroundColor: "#d3d3d3",
             width: "100vw",
-            m: 0,
-            p: 0,
             borderTopLeftRadius: "40%",
             borderBottomLeftRadius: "50%",
-            //zIndex: 9,
-            //border:'solid',
             boxShadow: "2px 0px 2px 2px rgba(0, 0, 0, 0.25)",
             display: "flex",
             flexDirection: "row",
-            //justifyContent: "center",
-            // alignItems: "stretch",
-            //alingContent: "space-around",
             alignContent: "center",
             flexWrap: "wrap",
             pl: "10%",
@@ -130,15 +148,16 @@ function App() {
                 sx={{
                   borderRadius: "50%",
                   backgroundColor: "#fff",
-                  // textAlign: "center",
-                  //boxShadow: "2px 2px 2px 2px rgba( 0,0,0, 0.25)",
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
-                  fontSize: "22px",
-                  fontWeight: "bold",
+                  fontSize: "27px",
+                  fontWeight: 700,
                   width: "5rem",
                   height: "5rem",
+                  fontFamily: "Open Sans",
+                  fontStyle: "normal",
+                  lineHeight: "33px",
                 }}
               >
                 {numero}
---
+++ modulo6/case-brainn/src/constants/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  /*     palette: {
+        primary: {
+            //main: primaryColor,
+           // contrastText: 'white',
+
+        },
+        text:{
+            //primary:neutraColor
+        }
+    } */
+  typography: {
+    fontFamily: ["Open Sans"],
+  },
+});
+export default theme;
